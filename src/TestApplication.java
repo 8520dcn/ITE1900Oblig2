ad5916622c69b4673ca7f6735883e11d08f31535
Dag Christian Nygaard <dag_christian_nygaard@hotmail.com>
2016-02-26 23:31:37 +0100

    Version 4

--- src/TestApplication.java
+++ src/TestApplication.java
@@ -5,13 +5,20 @@ public class TestApplication {
 		ArrayList<Card> kortArray = new ArrayList<>();
 		Card k1 = new Ansatt("Ole", "Olsen");
 		Card k2 = new Gjest("Marit", "Olsen");
+		Card k3 = new Gjest("Dag", "Nygaard");
 		kortArray.add(k1);
 		kortArray.add(k2);
+		kortArray.add(k3);
 		for (Card kort : kortArray) {
 			System.out.println(kort);
 			System.out.println("Kode 1234 er " + (kort.checkPin("1234") ? "gyldig" : "ugyldig"));
 			System.out.println("Kode 9999 er " + (kort.checkPin("9999") ? "gyldig" : "ugyldig"));
 			System.out.println("Koden er : " + kort.pinCode);
+			System.out.println( "Navn: " +  kort.firstName);
+			System.out.println( "Navn: " +  kort.lastName);
+			System.out.println( "Navn Kompare Int: " +  kort.compareTo(k1) );
+			System.out.println( "Navn Kompare Int: " +  kort.compareTo(k2) );
+			System.out.println( "Navn Kompare Int: " +  kort.compareTo(k3) );
 		}
 	}
 }
